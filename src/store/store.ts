071fa64b25b6c3308ca9310a43ae9447d5b9e854
vitdien25 <huyltq.21it@vku.udn.vn>
2025-08-01 15:26:50 +0700

    feat: integrate student API for activity feed and course stats

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,16 +3,22 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import counterReducer from "./slices/student/counterSlice";
 import { authApi } from "@/services/authApi";
 import { coursesApi } from "@/services/coursesApi";
+import { studentApi } from "./slices/student/studentApi";
 
 export const makeStore = () => {
   const store = configureStore({
     reducer: {
       counter: counterReducer,
       [authApi.reducerPath]: authApi.reducer,
+      [studentApi.reducerPath]: studentApi.reducer,
       [coursesApi.reducerPath]: coursesApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(authApi.middleware, coursesApi.middleware),
+      getDefaultMiddleware().concat(
+        authApi.middleware,
+        coursesApi.middleware,
+        studentApi.middleware
+      ),
   });
 
   // Enable listener behavior for the store
