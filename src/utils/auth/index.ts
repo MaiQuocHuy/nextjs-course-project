c552dcbc3091aa2b6250ba2b6991d2064e6776b7
mingttam <mingtam.713@gmail.com>
2025-08-13 20:54:13 +0700

    refactor: enhance authentication handling with helper functions

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -2,4 +2,5 @@ export * from "./decodeJWT";
 export * from "./isTokenExpired";
 export * from "./isRefreshTokenExpired";
 export * from "./isUnrecoverableError";
-export * from "./handleGoogleSignIn";
+// export * from "./handleGoogleSignIn";
+export * from "./authHandler";
